91b59a37ccc8e9984097f48b12faccd2c8c7c9f5
rpire <rubenpire7@gmail.com>
2022-03-26 18:04:17 -0400

    Incluir validaciones para productos

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,2 +1,9 @@
 class Producto < ApplicationRecord
+  belongs_to :usuario
+
+  validates :nombre, :descripción, :precio, presence: true
+  validates :nombre, length: { maximum: 50 }
+  validates :descripción, length: { maximum: 250 }
+  validates :precio, numericality: { greater_than_or_equal_to: 0 }
+  validates :imágenes, length: { minimum: 1, maximum: 3 }
 end
